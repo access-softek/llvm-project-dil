427693727e0c9a088c509593f5570db1af1ecacd
Ilia Kuklin <ikuklin@accesssoftek.com>
2025-03-13 22:06:16 +0500

    [Parser] Check canonical types during type comparison (#24)
    The canonical type check is done in addition to user type check so that when the user types are identical, the returned result retains the user type.

--- lldb/source/ValueObject/DILParser.cpp
+++ lldb/source/ValueObject/DILParser.cpp
@@ -486,7 +486,8 @@ UsualArithmeticConversions(std::shared_ptr<ExecutionContextScope> ctx,
   }
 
   if (!is_comp_assign) {
-    assert(lhs->GetDereferencedResultType().CompareTypes(rhs->GetDereferencedResultType()) &&
+    assert(lhs->GetDereferencedResultType().GetCanonicalType().CompareTypes(
+               rhs->GetDereferencedResultType().GetCanonicalType()) &&
            "integral promotion error: operands result types must be the same");
   }
 
@@ -3409,6 +3410,12 @@ ASTNodeUP DILParser::BuildTernaryOp(ASTNodeUP cond, ASTNodeUP lhs,
     return std::make_unique<TernaryOpNode>(location, lhs_type, std::move(cond),
                                            std::move(lhs), std::move(rhs));
   }
+  // If operands have the same canonical type, use the canonical type.
+  if (lhs_type.GetCanonicalType().CompareTypes(rhs_type.GetCanonicalType())) {
+    return std::make_unique<TernaryOpNode>(
+        location, lhs_type.GetCanonicalType(), std::move(cond), std::move(lhs),
+        std::move(rhs));
+  }
 
   // If both operands have arithmetic type, apply the usual arithmetic
   // conversions to bring them to a common type.
@@ -3433,6 +3440,12 @@ ASTNodeUP DILParser::BuildTernaryOp(ASTNodeUP cond, ASTNodeUP lhs,
   if (lhs_type.CompareTypes(rhs_type)) {
     return std::make_unique<TernaryOpNode>(location, lhs_type, std::move(cond),
                                            std::move(lhs), std::move(rhs));
+  }
+  // Check if operands have the same canonical pointer type.
+  if (lhs_type.GetCanonicalType().CompareTypes(rhs_type.GetCanonicalType())) {
+    return std::make_unique<TernaryOpNode>(
+        location, lhs_type.GetCanonicalType(), std::move(cond), std::move(lhs),
+        std::move(rhs));
   }
 
   // If one operand is a pointer and the other is a nullptr or literal zero,
